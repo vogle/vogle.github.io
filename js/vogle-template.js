2880fe1201cacfbeac6258cbf1b90d26afb440b9
Sean IM <nes@vogle.com>
2013-05-24 01:37:04 +0900

    update js

---
+++ js/vogle-template.js
@@ -0,0 +1,21 @@
+/**
+ * vogle.com
+ * site template
+ * Date : 2013-05-23
+ * by nes
+ */
+ 
+VOGLE = VOGLE || {};
+VOGLE.Modules = VOGLE.Modules || {};
+
+VOGLE.Modules.ResizeManager = VOGLE.Modules.ResizeManager || function () {
+}
+
+VOGLE.Modules.GravatarManager = VOGLE.Modules.GravatarManager || function () {
+}
+
+VOGLE.Modules.FacebookManager = VOGLE.Modules.FacebookManager || function () {
+}
+
+VOGLE.Utils = VOGLE.Utils || function () {
+}
